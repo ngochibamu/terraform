6f08b1026087ef0b35c8a20f72b78ef4dd7f07e9
dchibamu <dchibamu@gmail.com>
2022-08-19 22:36:17 +0200

    Web instance and eip

--- prod.tf
+++ prod.tf
@@ -37,3 +37,25 @@ resource "aws_security_group" "prod_web" {
 		"Terrain" : true
 	}
 }
+
+resource "aws_instance" "prod_web" {
+	ami 			= "ami-0355fe9eeeb3b3343"
+	instance_type 	= "t2.nano"
+
+	vpc_security_group_ids = [ 
+		aws_security_group.prod_web.id
+	 ]
+
+	tags = {
+		"Terraform" : true
+	}
+}
+
+resource "aws_eip" "prod_web" {
+
+	instance = aws_instance.prod_web.id
+	
+	tags = {
+		"Terraform" : true
+	}
+}
