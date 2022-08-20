964b2d2aef68f4e823a720e75c22ba4b754f5d22
dchibamu <dchibamu@gmail.com>
2022-08-20 11:23:14 +0200

    Rectored to use autoscaling group

--- prod.tf
+++ prod.tf
@@ -51,37 +51,8 @@ resource "aws_security_group" "prod_web" {
 	}
 }
 
-resource "aws_instance" "prod_web" {
-	count			= 2
-
-	ami 			= "ami-0355fe9eeeb3b3343"
-	instance_type 	= "t2.nano"
-
-	vpc_security_group_ids = [ 
-		aws_security_group.prod_web.id
-	 ]
-
-	tags = {
-		"Terraform" : true
-	}
-}
-
-resource "aws_eip_association" "prod_web" {
-	instance_id 	= aws_instance.prod_web.0.id
-	allocation_id 	= aws_eip.prod_web.id
-
-}
-
-resource "aws_eip" "prod_web" {
-
-	tags = {
-		"Terraform" : true
-	}
-}
-
 resource "aws_elb" "prod_web" {
 	name			= "prod-web"
-	instances 		= aws_instance.prod_web.*.id
 	subnets 		= [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
 	security_groups = [aws_security_group.prod_web.id]
 
@@ -98,3 +69,36 @@ resource "aws_elb" "prod_web" {
 	}
 }
 
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0355fe9eeeb3b3343"
+  instance_type = "t2.micro"
+
+  tags = {
+	"Terraform" : true
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  #availability_zones = ["eu-west-1a","eu-west-1b"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+	key 				= "Terraform"
+	value 				= "true"
+	propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
+}
